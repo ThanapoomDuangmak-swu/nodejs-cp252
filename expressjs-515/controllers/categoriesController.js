e27ff456a914e281631024b472f7ef67d26e92c0
thanapoom <thanapoom.duangmak@g.swu.ac.th>
2026-02-20 05:06:30 +0000

    expressjs

---
+++ expressjs-515/controllers/categoriesController.js
@@ -0,0 +1,13 @@
+const categories = [];
+
+const getCategories = (req, res) => {
+  if (!categories || categories.length === 0) {
+    return res.json({ categories: ['unknown'] });
+  }
+
+  res.json({ categories });
+};
+
+module.exports = {
+  getCategories,
+};
